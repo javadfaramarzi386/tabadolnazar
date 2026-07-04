782cf90015d36bb2d6a0822914825add8ec1370a
javadfaramarzi386 <faramarzi@outlook.com>
2026-07-04 12:37:58 +0330

    1405-04-02-commit

--- forum_project/settings.py
+++ forum_project/settings.py
@@ -27,10 +27,9 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-dzh0^t5i#)cvq^u-gb^1to@8bp
 # اگر می‌خواهید کاملاً اجباری False باشد، خط زیر را از کامنت خارج کنید:
 
 
-DEBUG = True
+DEBUG = False
 
-# ALLOWED_HOSTS = ['.railway.app', '127.0.0.1', 'localhost']
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.railway.app', '127.0.0.1', 'localhost']
 
 # ========================================
 # اپلیکیشن‌ها
